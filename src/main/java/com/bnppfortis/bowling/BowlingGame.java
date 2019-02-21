dc43f1e657e5ddd6c286cea967b9451f2ef04287
“Krishna” <iamkrishv@gmail.com>
2019-02-21 09:13:53 +0100

    Formatted the test code and its dependency code

--- src/main/java/com/bnppfortis/bowling/BowlingGame.java
+++ src/main/java/com/bnppfortis/bowling/BowlingGame.java
@@ -2,47 +2,46 @@ package com.bnppfortis.bowling;
 
 public class BowlingGame {
 	private int roll = 0;
-	private int[] rolls =new int[21];
+	private int[] rolls = new int[21];
 
 	public void roll(int... rolls) {
-		for (int pinsDown: rolls) {
+		for (int pinsDown : rolls) {
 			roll(pinsDown);
 		}
 	}
-	
+
 	public void roll(int pinsDown) {
 		rolls[roll++] = pinsDown;
 	}
 
 	public int score() {
-		
-		int score =0;
-		int cursor =0;
-		
+
+		int score = 0;
+		int cursor = 0;
+
 		for (int frame = 0; frame < 10; frame++) {
-			if(isStrike(cursor)) {
-				score +=10 +rolls[cursor+1]+rolls[cursor+2];
+			if (isStrike(cursor)) {
+				score += 10 + rolls[cursor + 1] + rolls[cursor + 2];
 				cursor++;
+			} else if (isSpare(cursor)) {
+				score += 10 + rolls[cursor + 2];
+				cursor += 2;
+			} else {
+				score += rolls[cursor] + rolls[cursor + 1];
+				cursor += 2;
 			}
-			else if(isSpare(cursor)) { 
-				score +=10 +rolls[cursor+2];
-				cursor+=2;
-			}else {
-				score +=rolls[cursor] +rolls[cursor+1];
-				cursor+=2;
-			}
-			
+
 		}
 
 		return score;
 	}
 
 	public boolean isStrike(int cursor) {
-		return rolls[cursor]==10;
+		return rolls[cursor] == 10;
 	}
 
 	public boolean isSpare(int cursor) {
-		return rolls[cursor] + rolls[cursor+1] ==10;
+		return rolls[cursor] + rolls[cursor + 1] == 10;
 	}
 
 }
--- src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
+++ src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
@@ -19,32 +19,33 @@ public class BowlingGameTest {
 
 	@Test
 	public void scoreGutterGame() {
-		bowlingGame.roll(0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		bowlingGame.roll(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		assertThat(bowlingGame.score(), is(0));
 
 	}
 
 	@Test
 	public void scoreGameOfOnes() {
-		bowlingGame.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
+		bowlingGame.roll(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 		assertThat(bowlingGame.score(), is(20));
 
 	}
-	
+
 	@Test
 	public void scoreSpareFollowedByThree() {
-		bowlingGame.roll(5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		bowlingGame.roll(5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		assertThat(bowlingGame.score(), is(16));
 	}
 
 	@Test
 	public void scoreStrikeFollowedByThreeThenThree() {
-		bowlingGame.roll(10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+		bowlingGame.roll(10, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		assertThat(bowlingGame.score(), is(22));
 	}
+
 	@Test
 	public void scorePerfectGame() {
-		bowlingGame.roll(10, 10, 10 ,10, 10, 10, 10, 10, 10, 10, 10, 10);
+		bowlingGame.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 		assertThat(bowlingGame.score(), is(300));
 	}
 
