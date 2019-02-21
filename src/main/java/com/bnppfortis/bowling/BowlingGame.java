2df78cac77f24a3c23aff49710fab3416d6104c2
“Krishna” <iamkrishv@gmail.com>
2019-02-21 07:38:15 +0100

    Added new unit testcase scenario and also modified its dependency class

--- src/main/java/com/bnppfortis/bowling/BowlingGame.java
+++ src/main/java/com/bnppfortis/bowling/BowlingGame.java
@@ -1,14 +1,15 @@
 package com.bnppfortis.bowling;
 
 public class BowlingGame {
+	private int score = 0;
 
 	public void roll(int pinsDown) {
-		
+		score += pinsDown;
 	}
 
 	public int score() {
 
-		return 0;
+		return score;
 	}
 
 }
--- src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
+++ src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
@@ -17,6 +17,14 @@ public class BowlingGameTest {
 		 
 	}
 	
-	
+	@Test
+	public void  scoreGameOfOnes() {
+		BowlingGame bowlingGame= new BowlingGame();
+		for (int i = 0; i < 20; i++) {
+			bowlingGame.roll(1);
+		}
+		assertThat(bowlingGame.score(), is(20));
+		 
+	}
 
 }
