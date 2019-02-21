165ecc04b397497b37ebd75ca0617e44ba8609ec
“Krishna” <iamkrishv@gmail.com>
2019-02-21 09:27:20 +0100

    Added Negative Testcases for the BowlingGameTest

--- src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
+++ src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
@@ -21,19 +21,35 @@ public class BowlingGameTest {
 	public void scoreGutterGame() {
 		bowlingGame.roll(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		assertThat(bowlingGame.score(), is(0));
-
+	}
+	
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void scoreGutterGameWithExhaustedRolls() {
+		bowlingGame.roll(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		assertThat(bowlingGame.score(), is(0));
 	}
 
 	@Test
 	public void scoreGameOfOnes() {
 		bowlingGame.roll(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 		assertThat(bowlingGame.score(), is(20));
-
+	}
+	
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void scoreGameOfOnesWithExhaustedRolls() {
+		bowlingGame.roll(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+		assertThat(bowlingGame.score(), is(20));
 	}
 
 	@Test
 	public void scoreSpareFollowedByThree() {
 		bowlingGame.roll(5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		assertThat(bowlingGame.score(), is(16));
+	}
+	
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void scoreSpareFollowedByThreeWithExhaustedRolls() {
+		bowlingGame.roll(5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		assertThat(bowlingGame.score(), is(16));
 	}
 
@@ -43,9 +59,21 @@ public class BowlingGameTest {
 		assertThat(bowlingGame.score(), is(22));
 	}
 
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void scoreStrikeFollowedByThreeThenThreeWithExhaustedRolls() {
+		bowlingGame.roll(10, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		assertThat(bowlingGame.score(), is(22));
+	}
+	
 	@Test
 	public void scorePerfectGame() {
 		bowlingGame.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+		assertThat(bowlingGame.score(), is(300));
+	}
+	
+	@Test(expected=ArrayIndexOutOfBoundsException.class)
+	public void scorePerfectGameWithExhaustedRolls() {
+		bowlingGame.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 		assertThat(bowlingGame.score(), is(300));
 	}
 
