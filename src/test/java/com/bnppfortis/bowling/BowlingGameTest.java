eba326e312823d1024129b11009fad3391d45d81
“Krishna” <iamkrishv@gmail.com>
2019-02-21 07:52:16 +0100

    refactored the unit test case and removed the duplicate lines

--- src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
+++ src/test/java/com/bnppfortis/bowling/BowlingGameTest.java
@@ -3,28 +3,38 @@ package com.bnppfortis.bowling;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
-	 
-	@Test
-	public void  scoreGutterGame() {
-		BowlingGame bowlingGame= new BowlingGame();
-		for (int i = 0; i < 20; i++) {
-			bowlingGame.roll(0);
-		}
-		assertThat(bowlingGame.score(), is(0));
-		 
+
+	private BowlingGame bowlingGame;
+
+	@Before
+	public void setUp() {
+
+		bowlingGame = new BowlingGame();
+
 	}
-	
+
 	@Test
-	public void  scoreGameOfOnes() {
-		BowlingGame bowlingGame= new BowlingGame();
-		for (int i = 0; i < 20; i++) {
-			bowlingGame.roll(1);
-		}
+	public void scoreGutterGame() {
+		roll(20, 0);
+		assertThat(bowlingGame.score(), is(0));
+
+	}
+
+	@Test
+	public void scoreGameOfOnes() {
+		roll(20, 1);
 		assertThat(bowlingGame.score(), is(20));
-		 
+
+	}
+
+	public void roll(int times, int pinsDown) {
+		for (int i = 0; i < times; i++) {
+			bowlingGame.roll(pinsDown);
+		}
 	}
 
 }
